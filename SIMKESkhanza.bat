253b88ead44912d8cea4c3b2132b101114780435
Wahyu Setiawan <wahyusetiaaa@users.noreply.github.com>
2021-10-28 06:15:47 +0700

    Add files via upload

---
+++ SIMKESkhanza.bat
@@ -0,0 +1,5 @@
+title simkes khanza
+COLOR 0A
+SET PATH="%CD%"\jdk-15.0.2\bin\;%PATH%.
+SET JAVA_HOME=%CD%"\jdk-15.0.2\;%JAVA_HOME%.
+java -jar -Xss2m -Xms32m -Xmx1024m -XX:PermSize=32m -XX:MaxPermSize=512m SIMKESKhanza.jar
